b2ed9b73766ef83e60c10090a638491d64a8600d
yonatan <yonatanafewerk@gmail.com>
2022-02-23 15:48:04 +0300

    Feb142022

--- C++/Class/examples/Done/try4.cpp
+++ C++/Class/examples/Done/try4.cpp
@@ -11,51 +11,9 @@
 
 using namespace std;
 
-struct address 
-{
-    int kebele;
-    char kefle_ketema[10];
-    char road_name[10];
-};
-
-
-struct student
-{
-    int age;
-    string name;
-    float grade;
-    //using a struct in another struct as a data type;
-    address stu_address;
-
-} a[10];
-
-
 int main()
 {
-    student stu1;
-
-    cout << "Enter Your Information: " << endl;
-        cout << "Student Name: ";
-        cin >> stu1.name;
-        cout << "Student Age: ";
-        cin >> stu1.age;
-        cout << "Student Grade: ";
-        cin >> stu1.grade;
-        cout << "Kebele: ";
-        cin >> stu1.stu_address.kebele;
-        cout << "Kefele ketama: ";
-        cin >> stu1.stu_address.kefle_ketema;
-
-        cout << endl;
- 
-
-    cout << stu1.stu_address.kebele;
-    cout << stu1.stu_address.kefle_ketema;
    
-
-    cout << a[2].name;
-
-
     // system("pause > 0");
     return 0;  
 }
